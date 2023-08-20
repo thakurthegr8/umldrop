3790343cb53bf78bd0243e8f7cd7b5778177f316
Rahultadmybrand <rahul.thakur@admybrand.com>
2023-08-20 23:43:41 +0530

    fixed mobile layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -25,15 +25,15 @@ export default function Home(props) {
         <Typography.Title className="font-bold text-center text-5xl lg:text-8xl tracking-tighter">⚡️Unleash Your SQL Mastery</Typography.Title>
         <Typography.Body className="text-center dark:text-secondary">Dive into the world of databases and query your way to excellence with our cutting-edge SQL coding platform. Elevate your skills, conquer complex data challenges, and shape your future as a database maestro.</Typography.Body>
         <Layout.Row className="justify-between w-full  backdrop:blur-2xl py-3 rounded-lg border border-secondary/30 bg-dark_secondary/50 px-4">
-          <Typography.Body className="font-semibold">Company</Typography.Body>
+          <Typography.Body className="font-semibold hidden md:flex">Company</Typography.Body>
           <Typography.Body className="font-semibold">Title</Typography.Body>
           <Typography.Body className="font-semibold">Difficulty</Typography.Body>
         </Layout.Row>
         <Layout.Col className="divide-y divide-dark_secondary ">
           {props.data && props.data.map((item, index) => {
             return item.category === "SQL" && !item.accessGroups ? <Link key={index} className="justify-between w-full py-2 px-4 flex hover:bg-dark_secondary/30 transition-all" href={`/questions/${item.slug}`}>
-              <Typography.Body className="text-left">{item.company}</Typography.Body>
-              <Typography.Body className="text-center flex-1">{item.title}</Typography.Body>
+              <Typography.Body className="text-left hidden md:flex">{item.company}</Typography.Body>
+              <Typography.Body className="text-left md:text-center flex-1">{item.title}</Typography.Body>
               <Badge type={item.difficulty.toLowerCase()}>{item.difficulty}</Badge>
             </Link> : null
           })}
