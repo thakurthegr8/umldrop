8d02043d736a5639ad913258a89786c0dab80666
Rahultadmybrand <rahul.thakur@admybrand.com>
2023-08-21 22:55:03 +0530

    added auth endpoints

--- src/services/supabase/index.js
+++ src/services/supabase/index.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseClient = createClient(
-  "https://svyypjsgedqqrrnxrxwr.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN2eXlwanNnZWRxcXJybnhyeHdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI2MzI1MTMsImV4cCI6MjAwODIwODUxM30.mMgS_QBW8NAwadYXeTOhWmdrEfTjFhWcFzYqEyem5NA",
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   { auth: { persistSession: false } }
 );
 
