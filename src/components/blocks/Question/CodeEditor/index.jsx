a39d20c3d705f74de61ac2e65729fcd6f476e54e
Rahultadmybrand <rahul.thakur@admybrand.com>
2023-08-27 21:36:45 +0530

    Added code theme customisation

--- src/components/blocks/Question/CodeEditor/index.jsx
+++ src/components/blocks/Question/CodeEditor/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { Editor, useMonaco } from '@monaco-editor/react';
 import Theme from "monaco-themes/themes/Blackboard.json";
 import Layout from '@/src/components/utils/Layout';
@@ -12,10 +12,12 @@ import useEditorTheme from '@/src/hooks/useEditorTheme';
 import Form from '@/src/components/utils/Form';
 import { Combobox } from '@headlessui/react';
 import { themes } from '@/src/constants/themes';
+import CustomComboBox from '@/src/components/utils/ComboBox';
 
 const CodeEditor = () => {
     const auth = useAuth();
     const monaco = useMonaco();
+    const theme = useLocalStorage({ fallback: "Monokai", key: "theme" });
     const question = useContext(QuestionContext);
     const editorHistory = useLocalStorage({ key: question.id, fallback: question?.baseQuery })
     const runCode = useFetch({ url: auth.data ? "/api/solution/run-auth" : "/api/solution/run", method: "POST" });
@@ -35,22 +37,24 @@ const CodeEditor = () => {
         }
         await runCode.dispatch(payload)
     }
-    const onChangeTheme = (value) => {
-        monaco && import(`monaco-themes/themes/${value}.json`).then((theme) => {
-            monaco.editor.defineTheme("my-theme", theme)
-        })
+    const onChangeTheme = async (value) => {
+        const currTheme = await import(`monaco-themes/themes/${value}.json`).then((mod) => mod);
+        monaco.editor.defineTheme("my-theme", currTheme);
+        monaco.editor.setTheme('my-theme');
+        theme.setItem("theme", value);
     }
+    const onEditorMount = async () => {
+        if (!monaco || !theme.value) return;
+        const currTheme = await import(`monaco-themes/themes/${theme.value}.json`).then((mod) => mod);
+        monaco.editor.defineTheme("my-theme", currTheme);
+        monaco.editor.setTheme('my-theme');
+    }
+    useEffect(() => {
+        onEditorMount();
+    }, [monaco]);
     return <>
         <Layout.Row className="p-2 relative">
-            <Combobox onChange={onChangeTheme} className="flex flex-col" as="div">
-                <Layout.Row>
-                    <Combobox.Input />
-                    <Combobox.Button>show</Combobox.Button>
-                </Layout.Row>
-                <Combobox.Options className="h-[200px] overflow-y-auto">
-                    {Object.keys(themes).map((item) => <Combobox.Option value={themes[item]}>{item}</Combobox.Option>)}
-                </Combobox.Options>
-            </Combobox>
+            <CustomComboBox placeholder="Select theme" list={Object.values(themes)} onChange={onChangeTheme} value="Your theme" />
         </Layout.Row>
         <Editor onChange={handleEditorChange} language="sql" height="100%" defaultValue={editorHistory.value ? editorHistory.value : question?.baseQuery} theme="my-theme" className="w-full" />
         <Layout.Col className="sticky bottom-0 ">
---
+++ src/components/utils/ComboBox/index.jsx
@@ -0,0 +1,45 @@
+import ProfileProgessProvider from '@/src/providers/Profile/Progress';
+import { Combobox } from '@headlessui/react';
+import ChevronUpDownIcon from '@heroicons/react/24/solid/ChevronUpDownIcon';
+import React, { useState } from 'react'
+import Layout from '../Layout';
+import Typography from '../Typography';
+
+const CustomComboBox = (props) => {
+    const [value, setValue] = useState(props.value);
+    const [query, setQuery] = useState("")
+    const onChange = (currValue) => {
+        setValue(currValue);
+        props.onChange(currValue);
+    }
+    const onInputChange = (e) => {
+        setQuery(e.target.value);
+    }
+    const filteredList = query === '' ? props.list : props.list.filter((item) => item.toLowerCase()
+        .replace(/\s+/g, '')
+        .includes(query.toLowerCase().replace(/\s+/g, '')));
+
+    return (
+        <Combobox value={value} onChange={onChange} as={Layout.Col} className="relative z-10 rounded-md border border-dark_secondary bg-dark_secondary/50">
+            <Layout.Row className="rounded overflow-hidden">
+                <Combobox.Input placeholder={props.placeholder} className="border-none bg-transparent" displayValue={(item) => item} onChange={onInputChange} />
+                <Combobox.Button><ChevronUpDownIcon className='w-6 h-6' /></Combobox.Button>
+            </Layout.Row>
+            <Combobox.Options onBlur={() => setQuery("")} className="absolute max-h-[200px] overflow-y-scroll top-12 border border-dark_secondary divide-y divide-dark_secondary bg-black overflow-hidden inset-x-0 rounded-md">
+                {filteredList.length == 0 && query !== "" ? <Typography.Body>Nothing found</Typography.Body>
+                    :
+                    filteredList.map((item, index) => <Combobox.Option className="p-2 bg-dark_secondary/50 hover:bg-dark_secondary cursor-pointer" value={item} key={index}>{item}</Combobox.Option>)}
+
+            </Combobox.Options>
+        </Combobox>
+    )
+}
+
+export default CustomComboBox;
+
+CustomComboBox.defaultProps = {
+    placeholder: "Combobox placeholder",
+    list: ["item 1", "item 2"],
+    onChange: () => null,
+    value: "query",
+}
